eaf99538a6263039bfee22c56d545a00a2810f28
Erik Kaareng-Sunde <esu@enonic.com>
2015-02-26 17:59:38 +0100

    added some spaces..

--- xp-demo/create_demo_instance.sh
+++ xp-demo/create_demo_instance.sh
@@ -31,20 +31,14 @@ function publish_demosite()
 	eval "curl -u $AUTH -H \"Content-Type: application/json\" -XPOST '$ADMIN_URL/rest/content/publish' -d '$JSON' "
 }
 
-
 echo "### Creating persistant storage container"
 docker run -it --name xp-home-demo enonic/xp-home
-
-
 
 echo "### Creating Enonic XP installation"
 docker run -d --volumes-from xp-home-demo --name xp-app-demo enonic/xp-app
 
-
 echo "### Starting up frontend"
 docker run -d --name xp-frontend -p 80:80 --link xp-app-demo:app enonic/xp-frontend
-
-
 
 echo "Sleeping for 20 seconds to make shure Enonic XP is up and running"
 sleep 20
@@ -53,14 +47,11 @@ echo "### Injecting demo module"
 docker exec xp-app-demo wget -O /tmp/demo-1.0.0.jar http://repo.enonic.com/public/com/enonic/xp/modules/demo/1.0.0/demo-1.0.0.jar
 docker exec xp-app-demo cp /tmp/demo-1.0.0.jar /enonic-xp/home/deploy/demo-1.0.0.jar
 
-
 echo "Sleeping for 20 seconds to get the demo deployment ready"
 sleep 20
 
 echo "### Publishing demo site"
 publish_demosite http://localhost/admin
-
-
 
 echo "### Setting up vhost properties"
 docker exec xp-app-demo wget -O /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg.template https://raw.githubusercontent.com/enonic/docker-xp/master/xp-demo/com.enonic.xp.web.vhost.cfg.template
@@ -68,15 +59,12 @@ docker exec xp-app-demo sed -i "s/HOSTNAME/$HOSTNAME/g" /enonic-xp/home/config/c
 docker exec xp-app-demo rm /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg
 docker exec xp-app-demo mv /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg.template /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg
 
-
-
-
 echo "### Changing su password to $PWD"
 set_password http://localhost/admin $PWD
 
-
-
+echo ""
 echo "### Finished configuring Enonic XP demo environment ###"
+
 echo "
 
 --- mail this part to customer ---
