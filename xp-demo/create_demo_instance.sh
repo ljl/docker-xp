e2214f47f63e1febe774f9fe45deea5254fbea28
Erik Kaareng-Sunde <esu@enonic.com>
2015-02-26 11:58:27 +0100

    enabled curl rest queries

--- xp-demo/create_demo_instance.sh
+++ xp-demo/create_demo_instance.sh
@@ -19,7 +19,7 @@ function set_password()
 
 	AUTH="su:password"
 	JSON="{\"key\":\"user:system:su\",\"password\":\"$PWD\"}"
-	echo "curl -u $AUTH -H \"Content-Type: application/json\" -XPOST '$ADMIN_URL/rest/security/principals/setPassword' -d '$JSON' "
+	eval "curl -u $AUTH -H \"Content-Type: application/json\" -XPOST '$ADMIN_URL/rest/security/principals/setPassword' -d '$JSON' "
 }
 
 function publish_demosite()
@@ -28,34 +28,39 @@ function publish_demosite()
 	
 	AUTH="su:password"
 	JSON="{\"ids\":[\"2dfbdc41-af98-4b3c-a2a9-9dc4814d003a\"]}"
-	echo "curl -u $AUTH -H \"Content-Type: application/json\" -XPOST '$ADMIN_URL/rest/content/publish' -d '$JSON' "
+	eval "curl -u $AUTH -H \"Content-Type: application/json\" -XPOST '$ADMIN_URL/rest/content/publish' -d '$JSON' "
 }
 
 
 echo "### Creating persistant storage container"
 docker run -it --name xp-home-demo enonic/xp-home
 
+echo " --- pause ---"
 read
 
 echo "### Creating Enonic XP installation"
 docker run -d --volumes-from xp-home-demo --name xp-app-demo enonic/xp-app
 
+echo " --- pause ---"
 read
 
 echo "### Starting up frontend"
 docker run -d --name xp-frontend -p 80:80 --link xp-app-demo:app enonic/xp-frontend
 
+echo " --- pause ---"
 read
 
 echo "### Injecting demo module"
 docker exec xp-app-demo wget -O /tmp/demo-1.0.0.jar http://repo.enonic.com/public/com/enonic/xp/modules/demo/1.0.0/demo-1.0.0.jar
 docker exec xp-app-demo cp /tmp/demo-1.0.0.jar /enonic-xp/home/deploy/demo-1.0.0.jar
 
+echo " --- pause ---"
 read
 
 echo "### Publishing demo site"
 publish_demosite http://$HOSTNAME/admin
 
+echo " --- pause ---"
 read
 
 echo "### Setting up vhost properties"
@@ -64,12 +69,14 @@ docker exec xp-app-demo sed -i "s/HOSTNAME/$HOSTNAME/g" /enonic-xp/home/config/c
 docker exec xp-app-demo rm /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg
 docker exec xp-app-demo mv /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg.template /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg
 
+echo " --- pause ---"
 read
 
 
 echo "### Changing su password to $PWD"
 set_password http://$HOSTNAME/admin $PWD
 
+echo " --- pause ---"
 read
 
 echo "### Finished configuring Enonic XP demo environment ###"
