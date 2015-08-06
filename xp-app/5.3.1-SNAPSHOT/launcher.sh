ba61783d7d4f259656961fc84bb2eca7a0d6d066
Glenn Ricaud <gri@enonic.com>
2015-08-06 12:26:48 +0200

    5.3.1-SNAPSHOT

---
+++ xp-app/5.3.1-SNAPSHOT/launcher.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+echo "Launching Enonic xp container..."
+echo "Setting filesystem permissions"
+
+# Extracting Enonic xp
+
+if [[ ! -d $XP_ROOT/bin ]]
+	then
+		echo "Removing osgi cache"
+		rm -rf $XP_HOME/work
+		echo "Downloading Enonic XP distro and extracting it to temp folder."
+		wget -O /tmp/distro-$XP_DISTRO_VERSION.tar.gz http://repo.enonic.com/public/com/enonic/xp/distro/$XP_DISTRO_VERSION/distro-$XP_DISTRO_VERSION.tar.gz
+		cd /tmp ; tar zxvf distro-$XP_DISTRO_VERSION.tar.gz
+		mv /tmp/enonic-xp-$XP_DISTRO_VERSION/home /tmp/enonic-xp-$XP_DISTRO_VERSION/home.org
+
+		cp -rf /tmp/enonic-xp-$XP_DISTRO_VERSION/* $XP_ROOT/.
+		rm -rf /tmp/distro-$XP_DISTRO_VERSION.tar.gz
+else
+	echo "Instance exists, continuing..."
+fi
+
+
+if [[ -d $XP_HOME ]]
+	then
+		for XP_DISTRO_DIR in $(ls -1 $XP_ROOT/home.org )
+			do
+				ABSOLUTE_XP_DISTRO_DIR=$XP_HOME/$XP_DISTRO_DIR
+				if [[ ! -d $ABSOLUTE_XP_DISTRO_DIR ]]
+					then
+					echo "$ABSOLUTE_XP_DISTRO_DIR does not exist, copying files from distro ( $XP_ROOT/home.org/$XP_DISTRO_DIR )..."
+					cp -r $XP_ROOT/home.org/$XP_DISTRO_DIR $ABSOLUTE_XP_DISTRO_DIR
+				fi
+			done
+
+	if [[ -f $XP_HOME/setenv.sh ]]
+		then
+		echo "Found custom setenv.sh file in home folder, copying it into runtime..."
+		rm $XP_ROOT/bin/setenv.sh
+		cp $XP_HOME/setenv.sh $XP_ROOT/bin/setenv.sh
+	fi
+
+else
+	echo "$XP_ROOT/home does not exist, copying files from distro."
+	cp -r $XP_ROOT/home.org $XP_HOME
+fi
+echo "Changing fileownership for files in $XP_ROOT to $XP_USER."
+chown -R $XP_USER $XP_ROOT
+
+echo "Starting Enonic xp..."
+cd $XP_ROOT/bin ; sudo su -c './server.sh $@' $XP_USER
