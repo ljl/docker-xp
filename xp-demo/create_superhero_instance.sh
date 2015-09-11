231a10f75ff8663f66c5fae7fa521263aa2945ba
Erik Kaareng-Sunde <esu@enonic.com>
2015-09-12 00:02:51 +0200

    updated superhero demo instance with docker-compose setup and Enonic XP 6.0.0

--- xp-demo/create_superhero_instance.sh
+++ xp-demo/create_superhero_instance.sh
@@ -9,7 +9,7 @@ if [[ "x$1" = "x" ]]
 	MY_HOSTNAME="$HOSTNAME.tryme.enonic.io"
 fi
 
-PWD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c14)
+PASSWD=$(< /dev/urandom tr -dc A-Za-z0-9 | head -c14)
 
 function set_password()
 {
@@ -21,58 +21,37 @@ function set_password()
 	eval "curl -u $AUTH -H \"Content-Type: application/json\" -XPOST '$ADMIN_URL/rest/security/principals/setPassword' -d '$JSON' "
 }
 
-function publish_demosite()
+function publish_superhero()
 {
 	ADMIN_URL=$1
 	
 	AUTH="su:password"
-	JSON="{\"ids\":[\"e1f57280-d672-4cd8-b674-98e26e5b69ae\"]}"
+	JSON="{\"ids\":[\"e1f57280-d672-4cd8-b674-98e26e5b69ae\"],\"includeChildren\":true}"
 	eval "curl -u $AUTH -H \"Content-Type: application/json\" -XPOST '$ADMIN_URL/rest/content/publish' -d '$JSON' "
 }
 
-echo "### Pulling docker images"
-echo "    - enonic/xp-home"
-docker pull enonic/xp-home
+echo "Creating Enonic XP superhero service"
+git clone https://github.com/enonic-io/docker-compose-enonic-xp-superhero.git /srv/superhero
+cd /srv/superhero
+./configure.sh $MY_HOSTNAME
 
-echo "    - enonic/xp-app"
-docker pull enonic/xp-app
+echo "Launching a superhero"
+docker-compose build
+docker-compose up -d
 
-echo "    - enonic/xp-frontend"
-docker pull enonic/xp-frontend
+echo "Sleeping for 40 seconds to get the demo deployment ready"
+sleep 40
 
-echo "### Creating persistant storage container"
-docker run -d -it --name xp-home-demo enonic/xp-home
-docker wait xp-home-demo
-echo "### Creating Enonic XP installation"
-docker run -d --volumes-from xp-home-demo --name xp-app-demo enonic/xp-app
-sleep 5
-echo "### Starting up frontend"
-docker run -d --name xp-frontend -p 80:80 --link xp-app-demo:app enonic/xp-frontend
+echo "Setting up hostfile to point $MY_HOSTNAME to localhost"
+echo "127.0.66.1 $MY_HOSTNAME" >> /etc/hosts
 
-echo "Sleeping for 20 seconds to make shure Enonic XP is up and running"
-sleep 20
-
-echo "### Injecting superhero module"
-
-MODULE_VERSION=0.1.0-SNAPSHOT
-
-docker exec xp-app-demo wget -O /tmp/superhero-$MODULE_VERSION.jar http://repo.enonic.com/public/com/enonic/theme/superhero/$MODULE_VERSION/superhero-$MODULE_VERSION.jar
-docker exec xp-app-demo cp /tmp/superhero-$MODULE_VERSION.jar /enonic-xp/home/deploy/superhero-$MODULE_VERSION.jar
-
-echo "Sleeping for 20 seconds to get the demo deployment ready"
-sleep 20
 
 echo "### Publishing demo site"
-publish_demosite http://localhost/admin
+publish_superhero http://$MY_HOSTNAME/admin
 
-echo "### Setting up vhost properties"
-docker exec xp-app-demo wget -O /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg.template https://raw.githubusercontent.com/enonic/docker-xp/master/xp-demo/com.enonic.xp.web.vhost.cfg.template-superhero
-docker exec xp-app-demo sed -i "s/HOSTNAME/$MY_HOSTNAME/g" /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg.template
-docker exec xp-app-demo rm /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg
-docker exec xp-app-demo mv /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg.template /enonic-xp/home/config/com.enonic.xp.web.vhost.cfg
-
-echo "### Changing su password to $PWD"
-set_password http://localhost/admin $PWD
+echo "### Changing su password to $PASSWD"
+set_password http://$MY_HOSTNAME/admin $PASSWD
+echo "$PASSWD" > /xp_su_pwd.txt
 
 echo ""
 echo "### Finished configuring Enonic XP demo environment ###"
@@ -82,25 +61,42 @@ echo "
 --- mail this part to customer ---
 Subject: Enonic XP demo instance
 
-Hi.
-I see that you have requested a demo installation of Enonic XP.
-You can access it here:
+Hi <name>!
+
+Thank you for signing up for an Enonic XP cloud trial.
+
+We have created a private installation in our Developer Cloud for your testing. You can access the installation here:
 
 Public site: http://$MY_HOSTNAME/
 Admin: http://$MY_HOSTNAME/admin 
 Username: su
-password: $PWD
+password: $PASSWD
 
-For documentation, please see https://enonic.com/docs/latest/
-And if there are any questions, please contact either Kristian or me ( I've added Kristian on cc. )
+Community forum:
+https://discuss.enonic.com/
 
-We'll keep the installation up for three days for you. If you wan't to keep it longer, just let us know. 
+Documentation:
+http://xp.readthedocs.org/en/6.0/
 
-" > /home/user/demo-instance.txt
+Training courses and more:
+https://enonic.com/learn
+
+Your installation is available for three days. Just, let us know if you need it longer!
+
+Finally, if you need any help let us know. We can also schedule a GotoMeeting to demo and answer questions.
+
+Vennlig hilsen/Best regards 
+
+Morten Øien Eriksen 
+CEO & Co-Founder 
+
+The fastest way from idea to Digital Experience - http://youtu.be/cFfxuWUgcvI
+
+" > /demo-instance.txt
 
 # Installing mailutils to send mail
 echo "postfix postfix/mailname string $(hostname -f)" | debconf-set-selections
 echo "postfix postfix/main_mailer_type string 'Internet Site'" | debconf-set-selections
 apt-get install -y mailutils
 
-cat /home/user/demo-instance.txt | mail -s "new Tryme installation ready on $HOSTNAME" tryme-admins@enonic.io
+cat /demo-instance.txt | mail -s "new Tryme installation ready on $HOSTNAME" tryme-admins@enonic.io
